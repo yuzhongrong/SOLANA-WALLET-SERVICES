2a8002ad00bd52e3ff2d8ea8597d61de7f766510
yuzhongrong <yuzhongrong2018@gmail.com>
2024-08-28 13:23:17 +0800

    提交分类接口测试

--- wallet/solanawallet/rpc/ave_rpc/category_hots.ts
+++ wallet/solanawallet/rpc/ave_rpc/category_hots.ts
@@ -227,7 +227,7 @@ export async function fetchTrendingTokens(category:string) {
                 }
                 return item;
             }));
-            // console.log("----最终结果--->"+categoryPairs.length+" "+JSON.stringify(categoryPairs))
+            console.log("----最终结果--->"+categoryPairs.length+" "+JSON.stringify(categoryPairs))
            
        
         
