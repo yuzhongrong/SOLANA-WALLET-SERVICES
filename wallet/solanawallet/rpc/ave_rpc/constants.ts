5405197cc2ed697cb6d0edaf68bced6095f17678
yuzhongrong <yuzhongrong2018@gmail.com>
2024-08-30 12:43:45 +0800

    增加域名

--- wallet/solanawallet/rpc/ave_rpc/constants.ts
+++ wallet/solanawallet/rpc/ave_rpc/constants.ts
@@ -1,26 +1,22 @@
 
- enum CATEGORYS_URL {
-    //热门
-    TRENDING = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=hot&refresh_total=0`,
-    //新开盘
-    NEW = 'https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&sort=created_at&sort_dir=desc&tvl_min=10000&pageNO=1&pageSize=30&category=new',
-    //涨幅榜
-    GAINER = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&sort=price_change_24h&sort_dir=desc&pageNO=1&pageSize=30&category=gainer`,
-    //新收录
-    INCLUSION = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&sort=target_listing_at&sort_dir=desc&pageNO=1&pageSize=30&category=inclusion`,
-   
-    //金狗
-    GOLDENDOG = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=goldendog&refresh_total=0`,
-    //潜力狗
-    ALPHA = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=alpha`,
-   
-    //PUMP热内盘
-    PUMP_IN_HOT='https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_in_hot',
-    //pump热外盘
-    PUMP_OUT_HOT = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_out_hot`,
-    //pump即将满盘
-    PUMP_IN_ALMOST = `https://api.fgsasd.org/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_in_almost`,
-}
+ 
+ const url="https://febweb002.com"
+
+ const BASE_URL: string = "https://febweb002.com";
+
+ const CATEGORYS_URL = {
+     TRENDING: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=hot&refresh_total=0`,
+     NEW: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&sort=created_at&sort_dir=desc&tvl_min=10000&pageNO=1&pageSize=30&category=new`,
+     GAINER: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&sort=price_change_24h&sort_dir=desc&pageNO=1&pageSize=30&category=gainer`,
+     INCLUSION: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&sort=target_listing_at&sort_dir=desc&pageNO=1&pageSize=30&category=inclusion`,
+     GOLDENDOG: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=goldendog&refresh_total=0`,
+     ALPHA: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=alpha`,
+     PUMP_IN_HOT: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_in_hot`,
+     PUMP_OUT_HOT: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_out_hot`,
+     PUMP_IN_ALMOST: `${BASE_URL}/v1api/v4/tokens/treasure/list?chain=solana&pageNO=1&pageSize=30&category=pump_in_almost`,
+ };
+
+ 
 
 export enum CATEGORYS{
     TRENDING='hot',
